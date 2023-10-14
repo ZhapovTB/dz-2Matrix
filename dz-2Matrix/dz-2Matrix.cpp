6c0c298f9c479563d869af8aa2b8113ffaaafe40
ZhapovTB <tizhapov@mail.ru>
2023-10-14 23:47:11 +1000

    Добавьте файлы проекта.

---
+++ dz-2Matrix/dz-2Matrix.cpp
@@ -0,0 +1,56 @@
+﻿#include <numeric>
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n , m , ch=1;
+    
+    std::cout << "Кол-во строк и столбцов: ";
+    std::cin >> n >> m;
+    while (std::cin.fail() || (n <= 0 || m <=0 )){
+        std::cin.clear();
+        std::cout << "Введите числа вверно! Повторите попытку: " << std::endl;
+        std::cin.ignore(std::numeric_limits < std::streamsize >::max(), '\n');
+        std::cin >> n>> m;
+    }
+    
+
+    int** arr = new int*[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = new int[m];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+             arr[i][j] = rand()%3;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+
+            std::cout << arr[i][j] << " ";
+            
+        }
+        std::cout<<std::endl;
+    }
+    std::cout << '\n';
+
+      
+    int* B = new int[m];
+    for (int i=0; i < m; i++) {
+        int total = std::accumulate(arr, arr + n, 0, [i](int a, int* b) {
+            return a + b[i];
+            });
+        if (total == 0) B[i] = 0;
+        else B[i] = 1;
+    }
+    
+   
+    for (int i = 0; i < m; i++) std::cout << B[i] << " ";
+    std::cout << " = B";
+  
+}
+
